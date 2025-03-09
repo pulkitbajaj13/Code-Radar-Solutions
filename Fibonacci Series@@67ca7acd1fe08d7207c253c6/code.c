9808d76f081d4159c31250eabd4028add8ae61e7
Pulkit Bajaj <pulkitbajaj13@gmail.com>
2025-03-09 18:09:00 +0530

    Solution to Fibonacci Series

--- Fibonacci Series@@67ca7acd1fe08d7207c253c6/code.c
+++ Fibonacci Series@@67ca7acd1fe08d7207c253c6/code.c
@@ -1,20 +1,13 @@
-
-
-// Function to return the nth Fibonacci number
-int fibonacciSeries(int n) {
-    if (n == 0) {
+fibonacciSeries(n){
+    for(int i=0;i<n;i++){
+        printf("%d ",fibonacciSeries(i));
+    }
+    if(n==0){
         return 0;
     }
-    if (n == 1) {
+    if(n==1){
         return 1;
     }
-    return fibonacciSeries(n - 1) + fibonacciSeries(n - 2);
+    return fibonacciSeries(n-1) + fibonacciSeries(n-2);
 }
-
-    // Print Fibonacci series up to 'n' terms
-    for (int i = 0; i < n; i++) {
-        printf("%d ", fibonacciSeries(i));
-    }
-    
-    return 0;
 
