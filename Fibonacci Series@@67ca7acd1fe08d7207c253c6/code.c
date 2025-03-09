84952a2a6dab573468abccdb83bd3cd8cc1caf4f
Pulkit Bajaj <pulkitbajaj13@gmail.com>
2025-03-09 18:03:57 +0530

    Solution to Fibonacci Series

--- Fibonacci Series@@67ca7acd1fe08d7207c253c6/code.c
+++ Fibonacci Series@@67ca7acd1fe08d7207c253c6/code.c
@@ -11,22 +11,10 @@ int fibonacciSeries(int n) {
     return fibonacciSeries(n - 1) + fibonacciSeries(n - 2);
 }
 
-int main() {
-    int n;
-
-    // Read the number of terms
-    scanf("%d", &n);
-
-    // Validate input
-    if (n <= 0) {
-        printf("Invalid input\n");
-        return 0;
-    }
-
     // Print Fibonacci series up to 'n' terms
     for (int i = 0; i < n; i++) {
         printf("%d ", fibonacciSeries(i));
     }
     
     return 0;
-}
+
