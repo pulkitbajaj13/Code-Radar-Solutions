e0901ff3ee31d69520983e64abdf6e65c17da288
Pulkit Bajaj <pulkitbajaj13@gmail.com>
2025-04-07 09:48:09 +0530

    Solution to Balloon Deflation Game

---
+++ Balloon Deflation Game@@67de00956ac3fec9afb90e15/code.c
@@ -0,0 +1,16 @@
+// Your code here...
+#include <stdio.h>
+
+int kthSmallest(int arr[],int n,int k){
+    int temp;
+    for(int i=0;i<n;i++){
+        for(int j=i+1;j<n;j++){
+            if(arr[i]>arr[j]){
+                temp=arr[i];
+                arr[i]=arr[j];
+                arr[j]=temp;
+            }
+        }
+    }
+    return arr[k-1];
+}
